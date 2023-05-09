ef4571e9a3d17624ce82298da537d34ef68b352f
Suruthi Kumar <suruthik2000@gmail.com>
2023-05-09 16:43:29 +0530

    methodcalling.java

--- Methodcalling/src/methodcalling/Methodcalling.java
+++ Methodcalling/src/methodcalling/Methodcalling.java
@@ -1,10 +1,15 @@
 package methodcalling;
-
 public class Methodcalling {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int addition(int num1, int num2) {
+		int sum = num1+num2;
+		return sum;
 	}
 
+	public static void main(String[] args) {
+			int a = 20;
+			int b = 10;
+			Methodcalling obj = new Methodcalling();
+			int result = obj.addition(a, b);
+			System.out.print(result);
+		}
 }
